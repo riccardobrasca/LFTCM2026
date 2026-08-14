41cc86506fdd148a7ffdf80df3df7c93a48ffaa5
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-08-14 20:54:41 +0200

    better

--- LFTCM2026/Test.lean
+++ LFTCM2026/Test.lean
@@ -6,7 +6,7 @@ Authors: Riccardo Brasca
 
 module
 
-import Mathlib.Tactic
+import LFTCM2026.Preliminaries.Test
 
 /- This is a test file. Lean is configured correctly if you see the output "32" when
   mousing over or clicking on the next line, and you see no other errors in this file. -/
