3b583d422c3b75735a9ba47d40b0f8fe71706f71
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2026-08-31 20:09:46 +0200

    added files

---
+++ LFTCM2026/5_StructuresAndClasses.lean
@@ -0,0 +1,316 @@
+import Mathlib
+
+/- # Structures and Classes:
+Before embarking on today's class, let's see how things might *go wrong*, so that the design
+choices will hopefully become clearer. -/
+
+section Examples
+
+-- ### Some crazy stuff
+
+
+example {G : Type*} [CommGroup G] (N : Subgroup G) : CommGroup (G ⧸ N) := by
+  sorry
+  done
+
+/- `Lean` is not so stupid after all, it understands that metric spaces have a topology...
+but how is this possible? -/
+example {X Y : Type*} [MetricSpace X] [MetricSpace Y] [Group Y] [IsTopologicalGroup Y]
+    (f g h : X → Y) : Continuous f → Continuous g → Continuous h → Continuous (f * g / h) := by
+  sorry
+  done
+
+/- Can you understand the error message (I'm not asking whether you understand *why* you get
+the error: just what it says). -/
+lemma quotComm_lemma {G : Type*} [Group G] (N : Subgroup G) : CommGroup (G ⧸ N) := by
+  sorry
+  done
+
+-- This is false, but at least it compiles
+def quotComm_def {G : Type*} [Group G] (N : Subgroup G) : Group (G ⧸ N) := by
+  sorry
+  done
+
+-- And what goes on here?!?
+#print continuous_fst
+example (X Y : Type*) [TopologicalSpace X] [TopologicalSpace Y] [TopologicalSpace (X × Y)] :
+    Continuous (fun p : X × Y ↦ p.1) := by
+  sorry
+  done
+
+
+end Examples
+
+section Structures
+
+-- # A wrong way to define mathematical structures
+/- The leitmotiv here is that in "informal" mathematics, the way we aggregate data and conditions
+on these data is somewhat irrelevant. We can equally well say that
+* A group is a set `G` with a preferred element `e` and a binary operation such that...
+* A group law on a non-empty set `G` is the datum of a choice of some `e : G` and an operation ...
+* In any of the above, we can require that `e * g = g` **and** `g * e = g` for every `g`; or just
+one of the two, since one can prove that every right inverse is also a left inverse...
+All these are considered *the same mathematical structure*: the choice is almost "typographical".
+
+In a similar vein, we typically denote by `*` or `+` the operation, and in the first case we
+sometimes write `e = 1`, in the second `e = 0`: but it is "clear" that the choice of the symbol
+plays no role, and using `◆` and `e = §` would be good.
+
+In Lean we need to do things differently.
+-/
+
+structure WrongGroup where
+  carrier : Type*
+  one : carrier
+  mul : carrier → carrier → carrier
+  inv : carrier → carrier
+  mul_one : ∀ (x : carrier), mul x one = x
+  one_mul : ∀ (x : carrier), mul one x = x
+  mul_assoc : ∀ (x y z : carrier), mul (mul x y) z = mul x (mul y z)
+  inv_mul_cancel : ∀ (x : carrier), mul (inv x) x = one
+
+-- If this is how we play with products and inverses, we'd rather give up with `Lean` altogether...
+lemma WrongGroup.inv_eq_of_mul {α : WrongGroup} (x y : α.carrier) :
+    α.mul x y = α.one → α.inv x = y := by
+  sorry
+  done
+
+structure WrongSemigroup where
+  carrier : Type*
+  mul : carrier → carrier → carrier
+  mul_assoc : ∀ (x y z : carrier), mul (mul x y) z = mul x (mul y z)
+
+-- A `lemma` saying that `a * ((b * c) * d = (a * b) * (c * d)`
+lemma assoc_mul (X : WrongSemigroup) (a b c d : X.carrier) :
+    X.mul a (X.mul (X.mul b c) d) = X.mul (X.mul a b) (X.mul c d) := by
+  sorry
+  done
+
+/- If something is true in a Semigroup, it will stay so in a group; so the above `lemma` should
+still hold; yet...-/
+lemma assoc_mul' (G : WrongGroup) (a b c d : G.carrier) :
+    G.mul a (G.mul (G.mul b c) d) = G.mul (G.mul a b) (G.mul c d) := by
+  sorry
+  done
+
+/- Just to finish convincing ourselves that this `WrongXXX` approach is *wrong*, let's try to
+check that the usual addition makes `ℕ` into a `(Wrong)Semigroup`. -/
+def Nplus : WrongSemigroup where
+  carrier := sorry
+  mul := sorry
+  mul_assoc := sorry
+
+example : Nplus.mul 1 1 = 2 := rfl
+example : Nplus.mul (1 : Nplus) (1 : Nplus) = (2 : Nplus) := rfl
+example : Nplus.mul (1 : ℕ) (1 : ℕ) = (2 : ℕ) := rfl
+
+
+-- ## A right way to define mathematical structures
+
+/- We'll focus on Groups in what follows, but just for the sake of an example: their true theory
+will be discussed in the lecture about Algebra. -/
+#print Group
+-- and right-clicking on it yields (the `_in_Cortona` avoids Lean complaining this already exists)
+structure Group_in_Cortona (G : Type*) extends DivInvMonoid G where
+  protected inv_mul_cancel : ∀ a : G, a⁻¹ * a = 1
+
+#print DivInvMonoid
+
+
+-- ## Our old friends ∧ and ↔ are also structures
+#print And
+#print Iff
+
+example (P Q : Prop) : P ∧ Q → ((P → Q) ↔ (Q → P)) := by
+  sorry
+  done
+
+/- But how to *remember* the names and list of all fields in a structure? Of course
+one can have a look in the file where it is defined, but it's painful and might be very long...
+-/
+def DiscreteMetric (M : Type*) : MetricSpace M where
+  dist := sorry
+  dist_self := sorry
+  dist_comm := sorry
+  dist_triangle := sorry
+  edist_dist := sorry
+  uniformity_dist := sorry
+  cobounded_sets := sorry
+  eq_of_dist_eq_zero := sorry
+
+/- Actually, try typing just
+
+def DiscreteMetric (M : Type*) : MetricSpace M :=
+_
+
+and follow the bulb...💡️
+-/
+
+end Structures
+
+noncomputable section Classes
+open Classical
+
+-- ### Some magic happens!
+
+example (G : Type*) [Group G] (x y z : G) : x * y * z * 1 = x * (y * z) * 1 := by simp [mul_assoc]
+
+-- actually `mul_assoc` does not only work for groups, yet `Lean` was happy using it! *Why?*
+#check mul_assoc
+
+-- Similarly...
+lemma OneOne_Cortona {A : Type*} [Monoid A] (a : A) : a * 1 * 1 = a := by simp
+
+example {F : Type*} [NormedField F] (x : F) : x * 1 * 1 = x := by
+  sorry
+  done
+
+/-`Classes` are special `structures`, for which certain terms are stored in a database.
+
+Each `class` type cointains a *preferred* or a *canonical* term, declared using the keyword
+`instance` rather than `def`; and this term gets registered in the database to be accessible
+whenever needed. For example, we want that the terms of type `Field ℝ` or `TopologicalSpace ℂ`,
+representing respecitvely *a* field structure on `ℝ` and *a* topological structure on `ℂ`, are
+found automatically, and are always what we expect them to be.
+
+*Warning*: often, `Classes` have parameters, so if `G` and `H` are types, `Group G` and `Group H`
+are different types! In particular, you don't expect the "type" `TopologicalSpace` to have an
+instance (indeed, `TopologicalSpace` is not a type, it's a collection of types!): rather, you
+expect the type `TopologicalSpace ℝ`, that contains all possible topologies on the set `ℝ` to
+have an instance, and likely you want it to be the Euclidean topology.
+
+Classes also enable **class type inference**, constructing a term of a certain class given a term
+of a "parent" one. -/
+
+
+#synth Group ℤ
+#synth AddGroup ℤ
+#synth AddGroup (ℤ × ℤ)
+#synth TopologicalSpace ℂ
+
+def ℂ_Cortona := ℂ
+#synth TopologicalSpace ℂ_Cortona
+
+#print One
+#synth One ℤ
+
+example : AddGroup (ℤ × ℚ) := sorry
+
+-- Since finding instances is "automatic", the search can be used to create new ones:
+variable (X Y : Type) [TopologicalSpace X] [TopologicalSpace Y] in
+#synth TopologicalSpace (X × Y)
+
+variable (X Y : Type) [MetricSpace X] [MetricSpace Y] in
+#synth TopologicalSpace (X × Y) -- Lean went from `MetricSpace` to `TopologicalSpace` automatically.
+
+variable (X : Type) [MetricSpace X] in
+#synth MetricSpace (X × ℕ)
+#synth MetricSpace ℕ
+
+/- ...but not all the time, expecially if we don't have a canonical candidate: in the following,
+you can think at `((i : ι) → X i) = Π (i : ι), X_i` and `(i : ι) → MetricSpace (X i)` as a metric
+structure on each of the `X_i`'s.
+-/
+variable (ι : Type*) (X : ι → Type*) [(i : ι) → MetricSpace (X i)]
+#synth MetricSpace ((i : ι) → X i) -- indeed, `Lp` spaces exist for all `p`!
+
+variable (ι : Type*) (X : ι → Type*) [(i : ι) → Group (X i)]
+#synth Group ((i : ι) → X i)
+
+instance CortonaMetric (ι : Type*) (X : ι → Type*) [(i : ι) → MetricSpace (X i)] :
+    (MetricSpace ((i : ι) → X i)) where
+      dist x y := if x = y then 0 else 1
+      dist_self x := by grind
+      dist_comm x y := by grind
+      dist_triangle x y z := by grind
+      edist_dist x y := by split_ifs <;> simp_all
+      eq_of_dist_eq_zero {x} {y} := by grind
+
+variable (ι : Type*) (X : ι → Type*) [(i : ι) → MetricSpace (X i)]
+#synth MetricSpace ((i : ι) → X i)
+
+
+example {A : Type*} [AddGroup A] (x y : A) : x + y + 0 = x + y := by
+  sorry
+  done
+
+#print HAdd
+-- @[inherit_doc] infixl:65 " + "   => HAdd.hAdd
+
+
+-- What's going on here?
+example (G : Type*) [Group G] [CommGroup G] (g : G) : 1 * g = g := by
+  sorry
+  done
+
+instance : Add Bool where
+  add b₁ b₂ := b₁ && b₂
+
+example : true + false = false := by sorry
+
+end Classes
+
+-- # Exercises
+
+section Exercises
+
+-- **Exercise**
+-- Do you understand why the first of the next two lines compiles while the second
+-- throws an error?
+example (M : Type*) (α : Monoid M) : (1 : M) = (1 : M) := rfl
+example (α : Type*) (M : Monoid α) : (1 : M) = (1 : M) := rfl
+
+open Function
+
+/- **¶ Exercise**
+Re-experience the pain of playing with *wrongly-defined* groups. -/
+lemma WrongGroup.mul_inv_cancel {α : WrongGroup} (x : α.carrier) :
+    α.mul x (α.inv x) = α.one := by
+  sorry
+  done
+
+/- **¶ Exercise**
+Prove that in every additive group, the intersection of two normal subgroups is normal:
+even if you find a one-line proof, try to produce the whole term. For reasons to be explained later,
+the intersection is written `⊓` and types as `\inf`.
+
+Since we haven't discussed yet
+what a subgroup is, let alone a normal one, it can be useful to -/
+#print Subgroup.Normal --it's a class, so in particular a structure!
+
+example (A : Type*) [AddGroup A] (H K : AddSubgroup A) :
+    H.Normal → K.Normal → (H ⊓ K).Normal := by
+  sorry
+  done
+
+/- **¶ Exercise**
+You'll soon see how `Mathlib` defines topological spaces. For this exercise, we're implementing
+a toy model: a structure that I call *Cortological* spaces, where we just ask for the datum of a
+collection of "opens", satisfying no intersection/union property but just obeying the requirement
+that the empty set and the universal set belong to the collection. -/
+
+-- **1** Define the structure of a Cortological space on a type `X`: should this be a class?
+
+/- **2** Put a Cortological structure on `ℕ`, declaring that a non-empty set is "open" if it
+contains arbitrarily large elements, and then prove that the intersection of two opens is open:-/
+
+/- **3** Make sure that if `X` and `Y` are both `CortologicalSpaces`, Lean automatically puts a
+structure of `CortologicalSpace` on `X × Y`: I'm not asking any relation among this structure
+and the starting structures whatsoever... (remember that this is a *toy model*!): -/
+
+/- **4** Define the structure of punctured cortological spaces and discuss advantages and
+disadvantages of making it a class (think at the product of two punctured Cortological spaces). -/
+
+
+/- **¶ Exercise**
+Given a multiplicative group `G` and an additive group `A`, what is the right way of putting a
+multiplicative structure on `G × A` where `(g, a) * (h, b) = (g * h, a + b)`?
+**HINTS:** This might be hard: a sugggestion is to do it in steps, first defining the multiplication
+just as a function, then definining a `Mul` instance, then providing a trivial lemma saying
+how this multiplication behaves, and then providing the final instance (where you actually don't
+need to provide *all* fields, as some can be deduced automatically: try to comment them to see when
+`Lean` complains).
+-/
+
+
+end Exercises
---
+++ LFTCM2026/6_AlgebraNumberTheory.lean
@@ -0,0 +1,322 @@
+import Mathlib
+
+/- # Algebra (and Number Theory)
+The goal of today's lecture is to discuss some algebraic structures: (Sub)Groups, Rings and Vector
+Spaces. If time permits (and it won't) we might discuss a bit of modular arithmetic.
+-/
+
+section Groups
+
+/- ## Groups
+We've already seen in the last lecture how to *define* groups. Let's see how to play with them.
+-/
+
+example {G : Type*} [Group G] (x y z : G) : x * (y * z) * (x * z)⁻¹ * (x * y * x⁻¹)⁻¹ = 1 := by
+  sorry
+  done
+
+
+example {G : Type*} [CommGroup G] (x y : G) : (x * y)⁻¹ = x⁻¹ * y⁻¹ := by
+  sorry
+  done
+
+example {A : Type*} [AddCommGroup A] (x y : A) : x + y + 0 = x + y := by
+  sorry
+  done
+
+/- All this is very nice, but are we *duplicating* the whole library for both additive and
+multiplicative groups?. -/
+
+-- `⌘`
+lemma mul_square {G : Type*} [Group G] {x y : G} (h : x * y = 1) : x * y ^ 2 = y := by
+  sorry
+  done
+
+example {A : Type*} [AddGroup A] {a b : A} (h : a + b = 0) : a + 2 • b = b := by
+  sorry
+  done
+
+/- ### Subgroups
+Given all the stress I put in saying **we must use structures**, you might imagine how we
+begin to define what a subgroup is...
+-/
+#print Subgroup
+/-
+structure Subgroup.{u_3} (G : Type u_3) [Group G] : Type u_3
+number of parameters: 2
+parents:
+  Subgroup.toSubmonoid : Submonoid G
+fields:
+  Subsemigroup.carrier : Set G
+  Subsemigroup.mul_mem' : ∀ {a b : G}, a ∈ self.carrier → b ∈ self.carrier → a * b ∈ self.carrier
+  Submonoid.one_mem' : 1 ∈ self.carrier
+  Subgroup.inv_mem' : ∀ {x : G}, x ∈ self.carrier → x⁻¹ ∈ self.carrier
+constructor:
+  Subgroup.mk.{u_3} {G : Type u_3} [Group G] (toSubmonoid : Submonoid G)
+    (inv_mem' : ∀ {x : G}, x ∈ toSubmonoid.carrier → x⁻¹ ∈ toSubmonoid.carrier) : Subgroup G
+field notation resolution order:
+  Subgroup, Submonoid, Subsemigroup
+
+Concretely, there are *four* fields: a `carrier` (which is a set in `G`) and three proofs that
+this `carrier` is stable with respect to the operations. So **a subgroup is a quadruple**:
+-/
+variable (G : Type*) [Group G] (H : Subgroup G)
+#check H.carrier
+#check H.mul_mem'
+#check H.one_mem'
+#check H.inv_mem'
+
+-- And, hopefully, a subgroup is *also* a group:
+example (H : Subgroup G) : Group H := sorry
+
+variable (H : Subgroup G) in
+#synth Group H
+
+-- The following two examples seem to say something stupid, but for Lean it's an effort!
+example (H : Subgroup G) (x : H) (hx : x = 1) : (x : G) = 1 := by -- are the two `1`'s the same?
+  sorry
+  done
+
+example (H : Subgroup G) : 1 ∈ H := H.one_mem
+
+-- Let's define `2ℤ` as a term of `Subgroup ℤ`... (almost doable!)
+example : AddSubgroup ℤ := sorry
+
+/- In the example below, note two things:
+1. What happens if we remove `Comm`;
+2. What happens to the `G` and `Group G` that are globally defined in this section;
+-/
+example (G : Type*) [CommGroup G] (H₁ H₂ : Subgroup G) {x y : G} (hx : x ∈ H₁) (hy : y ∈ H₂) :
+    x * y ∈ H₁ ⊔ H₂ := by
+  sorry
+  done
+
+
+---Let's discuss **dot notation**.
+example : (Subgroup.center G).Normal := by
+  sorry
+  done
+
+/- ### Homomorphisms
+Once again, a homomorphism is a collection of things: a bare function, together with the properties
+that it must satisfy to be a group homomorphism. So, the *collection* of all these homomorphisms
+is a `structure`, denoted `G →* H`! Note that actually we define `monoid` homomorphisms, because a
+multiplicative map sending `1` to `1` automatically preserves the inverse. -/
+
+structure MonoidHom_Cortona (M N : Type*) [Monoid M] [Monoid N] where
+  toFun : M → N
+  map_one : toFun 1 = 1
+  map_mul : ∀ (x y : M), toFun (x * y) = (toFun x) * (toFun y)
+
+
+/- Two examples of things we can say about a morphism: -/
+example (G₁ : Type) [CommGroup G₁] (f : G →* G₁) : ∀ x y : G, x * y = 1 → (f x) * (f y) = 1 := by
+  sorry
+  done
+
+-- `⌘`
+example (A : Type*) [AddGroup A] (f : G →* A) : ∀ x y : G, x * y = 1 → (f x) * (f y) = 1 := by
+  sorry
+  done
+
+
+end Groups
+
+section Rings
+
+/- ## Rings
+The situation is very similar to the one for `Group`s: they're a class, and they have a dedicated
+tactic, `ring`: it closes every goal that holds in a *free commutative ring*. There is also `grind`
+that, on top of `ring` , is capable of performing (some) logical reasoning and arithmetic,
+including inequalities. -/
+
+#synth Ring ℤ
+#synth CommRing ℤ
+#synth Monoid ℤ
+#synth AddMonoid ℤ
+#check CommRing.toCommMonoid
+#check CommRing.toAddCommGroupWithOne
+
+example (a b c : ℚ) : a * (b + c * 1) = b * a + a * c := by
+  sorry
+  done
+
+
+#check mul_one
+#check add_assoc
+#print AddMonoid
+#print Field
+#print IsDomain
+#print CommMonoid
+
+
+example (R : Type*) [CommRing R] (x y : R) : (x + y) ^ 2 = x ^ 2 + 2 * x * y + y ^ 2 := by
+  sorry
+  done
+
+lemma sixth_pow (R : Type*) [CommRing R] (x y : R) : (x + y) ^ 6 =
+    x ^ 6 + 6 * x ^ 5 * y +  15 * x ^ 4 * y ^ 2 + 20 * x ^ 3 * y ^ 3 +
+      15 * x ^ 2 * y ^ 4 + 6 * x * y ^ 5 + y ^ 6 := by
+  sorry
+  done
+
+
+example (R : Type*) [CommRing R] (x y : R) (H : x ^ 2 ≠ y ^ 2) : x ≠ y := by
+  sorry
+  done
+
+variable {R S : Type*} [CommRing R] [CommRing S]
+
+example (f : R →+* S) (r : R) : IsUnit r → IsUnit (f r) := by
+  sorry
+  done
+
+end Rings
+
+section VectorSpaces
+
+/- We all agree that, at the end of the day, a vector space `V` over a field `K` is just a
+`K`-module, but it is much better to call it a *`K`-vector space* rather than *`K`-module`...
+
+Except that we **don't agree**.
+-/
+
+#check VectorSpace
+#check vectorSpace
+#check Vectorspace
+#check Module
+
+variable (K : Type*) [Field K]
+#print Module --it's a class
+
+variable (V : Type*) /- [AddCommGroup V]  -/[Module K V]
+--it *requires* that `V` be a(n additive, commutative) group:
+
+
+example (T : Submodule K V) (x y : V) (c : K) : x ∈ T → y ∈ T →
+  c * x + y ∈ T := sorry -- **why this does not work?**
+  done
+
+
+example (f : K →ₗ[K] K) : ∃ c, f = (fun x ↦ c • x) := by --what is going on?
+  sorry
+  done
+
+/- Let's try to state that "the collection of linear maps from `V` to `W` that vanish on a
+subspace `T ≤ V` form a linear/vector subspace of all linear maps.
+-/
+variable (W : Type*) [AddCommGroup W] [Module K W] in
+theorem Annihilator_Submodule (T : Subspace K V) :
+  Submodule K {f : V →ₗ[K] W | ∀ x ∈ T, f x = 0} := sorry
+
+
+end VectorSpaces
+
+
+section ZModn
+
+/- ##ZMod
+We don't have time in this class to discuss how *quotients* are defined, but you can easily imagine
+that everything exists (and probably will have something to do with `structure`s and `class`es...)
+Just to get a feeling, here are three small examples of results in `ℤ/nℤ`. -/
+
+lemma exists_b_mul_zero (a n : ℕ) (H : ¬ Nat.Coprime a n) : ∃ b : ℕ, (a * b : (ZMod n)) = 0 := by
+  sorry
+  done
+
+example (a n : ℕ) (H : ¬ Nat.Coprime a n) : ∃ b, (a * b : ℤ ⧸ (Ideal.span {(n : ℤ)})) = 0 := by
+  sorry
+  done
+
+example (a n : ℕ) (H : ¬ Nat.Coprime a n) : ∃ b : ℕ,
+    ((a * b) : ℤ ⧸ (Ideal.span {(n : ℤ)})) = 0 := by
+  sorry
+  done
+
+
+end ZModn
+
+
+noncomputable section Exercises
+open Function
+
+
+/- **¶ Exercise**
+Why is the following example broken? Fix its statement, then prove it. -/
+example (G : Type*) [Group G] (H₁ H₂ : Subgroup G) : Subgroup (H₁ ∩ H₂) := sorry
+
+
+-- **¶ Exercise**
+open Function in
+example (A : Type*) [AddGroup A] (f : A →+ ℤ) (hf : 1 ∈ f.range) : Surjective f := by
+  sorry
+  done
+
+
+/- **¶ Exercise**
+Prove the claim made in class that a monoid homomorphism between groups respects the inverse. -/
+example (G H : Type*) [Group G] [Group H] (f : MonoidHom G H) (x : G) : f x⁻¹ = (f x)⁻¹ := by
+  sorry
+  done
+
+-- **¶ Exercise**
+/- The kernel of a ring homomorphism is an ideal: what is an ideal is part of the exercise... -/
+def kernel (R S : Type*) [CommRing R] [CommRing S] (f : R →+* S) : Ideal R := sorry
+
+/- **¶ Exercise**
+State and that a group is commutative if and only if the map `x ↦ x⁻¹` is a group homomorphism:
+even if you find a one-line proof, try to produce the whole term. To get `⁻¹`, type `\-1`. It can
+be easier to split this `if and only if` statement in two declarations: are they both lemmas, both
+definitions, a lemma and a definition?. -/
+
+
+/- **¶ Exercise**
+Prove that the homomorphisms between commutative monoids have a structure of commutative monoid. -/
+example (M N : Type*) [CommMonoid M] [CommMonoid N] : CommMonoid (M →* N) := sorry
+
+
+/- **¶ Exercise**
+Prove that an injective and surjective group homomorphism is an isomorphism:
+but what's an isomorphism? -/
+def IsoOfBijective (G H : Type*) [Group G] [Group H] (f : G →* H)
+    (h_surj : Surjective f) (h_inj : Injective f) : G ≃* H := sorry
+
+
+/- **¶ Exercise**
+State and prove that the image of an ideal through a surjective ring homomorphism is an ideal. -/
+
+
+/- **¶ Exercise**
+This is the standard fact that an ideal containing a unit is the whole ring. -/
+example {R S : Type*} [CommRing R] [CommRing S] (I : Ideal R) (r : R) :
+    r ∈ I → IsUnit r → I = ⊤ := by
+  sorry
+  done
+
+
+/- **¶ Exercise**
+**State** and **show** that a linear map is injective if and only if it has trivial kernel: try
+first to do the whole proof by hand, and then to find it in Mathlib. -/
+
+-- **¶ Exercise**
+variable (K V W : Type*) [Field K] [AddCommGroup V] [AddCommGroup W] [Module K V] [Module K W] in
+/- Ok, this exists in the library: can we understand it in detail? In other words: write it
+down by hand after understanding what all this is about. -/
+example : SMul K (V →ₗ[K] W) := by
+  sorry
+  done
+
+
+
+-- **¶ Exercise**
+/- For the following exercise, you might need `erw` which is a stronger version of `rw`, in
+case you **really really* think that `rw` should work, but it's not working. Similarly, consider
+the tactic `rw_mod_cast` when you want to rewrite something but some `↑` is preventing you. All in
+all, the exercise is a bit of a fight around the problem that `ℕ` and `ℤ` differ. -/
+example (p q : ℕ) (hp : Nat.Prime p) (hq : Nat.Prime q) (hpq : p ≠ q) :
+    IsUnit (p : (ℤ ⧸ Ideal.span {(q : ℤ)})) := by
+  sorry
+  done
+
+
+end Exercises
