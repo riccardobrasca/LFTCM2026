b7b7cb87464d4731038d45e5911e10feb8edd0bf
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2026-09-03 11:20:26 +0200

    Revert "add copyright"
    This reverts commit 4a656c15488c6b02caa78007a5cc3bda77fde180.

--- LFTCM2026/5_StructuresAndClasses.lean
+++ LFTCM2026/5_StructuresAndClasses.lean
@@ -1,11 +1,3 @@
-/-
-Copyright (c) 2026 Filippo A. E. Nuccio. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Filippo A. E. Nuccio
--/
-
-module
-
 import Mathlib
 
 /- # Structures and Classes:
