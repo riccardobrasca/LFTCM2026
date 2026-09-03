7cf92fed947c1eff988ffe92923e5cad157a4950
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2026-09-03 11:23:06 +0200

    added copyright

--- LFTCM2026/6_AlgebraNumberTheory_solutions.lean
+++ LFTCM2026/6_AlgebraNumberTheory_solutions.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2026 Filippo A. E. Nuccio. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Filippo A. E. Nuccio
+-/
+
 import Mathlib
 
 /- # Algebra (and Number Theory)
