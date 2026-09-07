b15a794ce6763225c8915c25dc156ceedb2ed77d
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2026-09-07 14:08:42 +0200

    added Preliminaries

---
+++ LFTCM2026/Preliminaries/Test.lean
@@ -0,0 +1,13 @@
+/-
+Copyright (c) 2026 Riccardo Brasca. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Riccardo Brasca
+-/
+
+module
+
+public import Mathlib.Tactic
+
+public section
+
+lemma foo : 0 = 0 := rfl
